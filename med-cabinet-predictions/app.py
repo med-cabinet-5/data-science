d97acd70359c5c3c231c291e1c50c25083e64468
David <civilianemail@gmail.com>
2020-02-03 15:47:33 -0800

    Update app.py

--- med-cabinet-predictions/app.py
+++ med-cabinet-predictions/app.py
@@ -41,7 +41,7 @@ def create_app():
         return pred, pred_dict
     
 
-    @app.route('/json', methods=['GET'])
+    @app.route('/json', methods=['POST'])
     def root():
         req_data = request.get_json()
         our_string = req_data[USER_INPUT_STRING]
