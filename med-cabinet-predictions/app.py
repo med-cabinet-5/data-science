fe5d2ef8710fdb98cbbabd44c3c28ebc31862691
David <civilianemail@gmail.com>
2020-02-06 16:06:22 -0800

    Update app.py

--- med-cabinet-predictions/app.py
+++ med-cabinet-predictions/app.py
@@ -129,14 +129,14 @@ def create_app():
     app = Flask(__name__)
     CORS(app)
 
-    @app.route('/stats', methods=['GET'])
+    @app.route('/stats', methods=['POST'])
     def root():
         req_data = request.get_json()
         our_string = req_data["USER_INPUT_STRING"]
         output = pred_list2(our_string)
         return output
     
-    @app.route('/json', methods=['GET'])
+    @app.route('/json', methods=['POST'])
     def root2():
         req_data = request.get_json()
         our_string = req_data['USER_INPUT_STRING']
